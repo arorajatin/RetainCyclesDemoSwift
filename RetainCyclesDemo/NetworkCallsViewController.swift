21e8035d932147c96b865cefb0d3a718a6086d43
Jatin Arora <jatinarora269@gmail.com>
2017-07-06 12:31:02 +0530

    Add NetworkCallViewController for retain cycle demo

--- RetainCyclesDemo/NetworkCallsViewController.swift
+++ RetainCyclesDemo/NetworkCallsViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class NetworkCallsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    deinit {
+        print("NetworkCallsViewController dealloced")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.makeNetworkCall()
     }
 
+    func makeNetworkCall() {
+        
+        
+        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+        guard let url = URL(string:"https://hacker-news.firebaseio.com/v0/topstories.json") else {
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.allHTTPHeaderFields = ["Content-Type": "application/json"]
+        urlRequest.timeoutInterval = 10
+        
+        let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
+            self.printStories(data: data)
+        }
+        task.resume()
+    }
+    
+    func printStories(data: Data?) {
+        print("\(String(describing: data))")
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    
 }
