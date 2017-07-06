1bc63083a82357530b3e8719a18004307c90fd5c
Jatin Arora <jatinarora269@gmail.com>
2017-07-06 11:53:56 +0530

    Add retain cycle for demonstrating blocks

--- RetainCyclesDemo/BlocksViewController.swift
+++ RetainCyclesDemo/BlocksViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class BlocksViewController: UIViewController {
 
+    var constraintResolver: ConstraintResolver?
+    
+    deinit {
+        print("BlocksViewController dealloced")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        constraintResolver = ConstraintResolver(callback: { (finish) in
+            self.resolvedConstraints()
+        })
+        
+        constraintResolver?.addConstraint("TEST")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.async {
+            self.constraintResolver?.changeConstraint("TEST", isConstraintMet: true)
+        }
+        
     }
 
+    func resolvedConstraints() {
+        print("Constraint resolver has resolved constraints")
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
